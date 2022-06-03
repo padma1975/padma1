2227f16d025d8e572592e9e4a23ca52b2fe15c7a
padma1975 <103980789+padma1975@users.noreply.github.com>
2022-06-03 21:26:35 +0530

    Delete spirng-data directory

--- spirng-data/employee-service/src/main/java/com/amit/app/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.amit.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.amit.app.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
