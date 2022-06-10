5d648b6b7fecb06213dff3f8a8015ad3823f0b2d
padma1975 <103980789+padma1975@users.noreply.github.com>
2022-06-10 17:26:00 +0530

    Add files via upload

---
+++ Customer_Service-configserver/Customer_Service/src/main/java/com/example/demo/model/Customer.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import java.math.BigInteger;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+	
+	private Long customerId;
+	
+	private String customerName;
+	
+	private String email;
+	
+	private BigInteger mobileNo;
+	
+	private String city;
+	
+	private String status;
+
+}
